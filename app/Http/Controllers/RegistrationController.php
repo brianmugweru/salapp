a41c9ea1d126764df889310cd5fd9402e01c8727
Brian Mugweru <brianmugweru@gmail.com>
2018-02-09 20:59:57 +0300

    add store controller to add user fields to database and authenticate user according to role

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+use App\User;
+
+class RegistrationController extends Controller
+{
+    /*
+     * Controller for registration view page
+     */
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    /*
+     * Controller to store user credentials
+     * and Login user into session
+     *
+     */
+    public function store(Request $request)
+    {
+        //Validate the user
+        $this->validate(request(),User::$rules);
+
+        //Create  and save the user
+        $user = User::create([
+            'name'=>$request['name'],
+            'email'=>$request['email'],
+            'password'=>bcrypt($request['password']),
+            'role'=>$request['role'],
+        ]);
+
+        //Sign them in 
+        auth()->login($user);
+
+        //Redirect to homepage
+        if(\Auth::user('role') == "salon")
+            return redirect()->salon();
+        else
+            return redirect()->home();
+
+    }
+}
