2a764827befd7dae66493efe57a8bde0e8796e51
Brian Mugweru <brianmugweru@gmail.com>
2018-02-09 20:53:52 +0300

    add construct auth middleware to style and service controllers

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -8,6 +8,18 @@ use Illuminate\Http\Request;
 class ServiceController extends Controller
 {
     /**
+     * Create a new controller instance for authentication middleware.
+     *
+     * @return void
+     */
+ 
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
