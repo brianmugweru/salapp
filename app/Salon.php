08c8794cfbba8d2770869e3eea1a6996186da0ba
Brian Mugweru <brianmugweru@gmail.com>
2018-02-09 17:38:19 +0300

    Add relationship function to model class Salon

--- app/Salon.php
+++ app/Salon.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Salon extends Model
 {
-    //
+    public function styles()
+    {
+        return $this->hasMany('App\Style');
+    }
+
+    public function services()
+    {
+        return $this->hasMany('App\Service')
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
