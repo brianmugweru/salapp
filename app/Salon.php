d367b315b32de55931c44064bead1fc1c8cc2b7a
Brian Mugweru <brianmugweru@gmail.com>
2018-02-11 15:04:15 +0300

    Add protected fillable properties fields to fill data to db

--- app/Salon.php
+++ app/Salon.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Salon extends Model
 {
+
+    protected $fillable = [
+        'name', 'longitude', 'latitude', 'opening_time', 'closing_time', 'image', 'user_id'
+    ];
+
     public function styles()
     {
         return $this->hasMany('App\Style');
@@ -13,7 +18,7 @@ class Salon extends Model
 
     public function services()
     {
-        return $this->hasMany('App\Service')
+        return $this->hasMany('App\Service');
     }
 
     public function user()
