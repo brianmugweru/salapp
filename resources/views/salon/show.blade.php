feaa655847189cf36595d11eea16d8b15b959b85
Brian Mugweru <brianmugweru@gmail.com>
2018-02-12 17:32:11 +0300

    add link to go back to view salons on salons page

--- resources/views/salon/show.blade.php
+++ resources/views/salon/show.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Dashboard <span style="float:right"><a href="/salon">View Salons</a></span></div>
 
                 <div class="panel-body">
                     @if (session('status'))
