26a80c1c4755408a18beacb3e3c23086fb3a3ce7
Brian Mugweru <brianmugweru@gmail.com>
2018-02-13 21:02:38 +0300

    add react js component route paths to app component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,36 @@
 
 /**
  * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
+ * includes React and other helpers. It's a great starting point while
+ * building robust, powerful web applications using React + Laravel.
  */
 
 require('./bootstrap');
-
-window.Vue = require('vue');
-
 /**
- * Next, we will create a fresh Vue application instance and attach it to
+ * Next, we will create a fresh React component instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+require('./components/Example');
+require('./components/CreateStyle');
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory, hashHistory, Link } from 'react-router';
 
-const app = new Vue({
-    el: '#app'
-});
+import Example from './components/Example';
+import CreateStyle from './components/CreateStyle';
+import ViewStyles from './components/ViewStyles';
+import EditStyle from './components/EditStyle';
+ 
+if (document.getElementById('example')) {
+    render(
+        <Router history={browserHistory}>
+            <Route path="/style/:id" component={Example}>
+                <Route path={"/style/edit/:id/"} component={EditStyle} />
+                <Route path={"/style/:id/add-style"} component={CreateStyle} />
+                <Route path={"/style/:id/view-styles"} component={ViewStyles} />
+            </Route>
+        </Router>,
+        document.getElementById('example'));
+}
