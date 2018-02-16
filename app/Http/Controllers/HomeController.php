ea37956e0c4b6d49200d2906ea11d44eb5288653
Brian Mugweru <brianmugweru@gmail.com>
2018-02-16 20:38:54 +0300

    add view to salons, single salons and like for salons

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Salon;
+
+use App\User;
+use App\Like;
+
 class HomeController extends Controller
 {
    /**
@@ -13,6 +18,35 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('welcome');
+        $salons = Salon::all();
+
+        return view('welcome')->withSalons($salons);
+    }
+
+    public function getsalon($id)
+    {
+        $salon = Salon::find($id);
+
+        return view('single')->withSalon($salon);
+    }
+
+    public function like($salon_id, $user_id)
+    {
+        $salon = Salon::find($salon_id);
+
+        $user = User::find($user_id);
+
+        $salon->addRank();
+    
+        $like = new Like;
+
+        $like->user_id = $user_id;
+
+        $like->salon_id = $salon_id;
+
+        $like -> save();
+
+        return redirect('/salon/2/book');
+
     }
 }
