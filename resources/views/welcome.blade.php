c8e76561cf8f76017b2ea671b803f2ed1693d25f
Brian Mugweru <brianmugweru@gmail.com>
2018-02-16 20:43:14 +0300

    add salons view to index page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,97 +1,17 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        @if (Auth()->User()->role == "salon")
-                            <a href="{{ url('/salon') }}">Dashboard</a>
-                        @endif
-                        <a href="{{ Route('logout') }}">Logout</a>
-                    @else
-                        <a href="{{ Route('login') }}">Login</a>
-                        <a href="{{ Route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+@section('content')
+    <div class="container">
+        <div class="row">
+            @foreach($salons as $salon)
+            <div class="col-sm-3">
+                <a href="/salon/{{ $salon->id }}/book">
+                    <img src="{{ Storage::url($salon->image) }}" width="100%" height="200">
+                    <p style="text-align:left;">{{ $salon->name }}</br> {{ $salon->rank }}</p>
+                </a>
             </div>
+            @endforeach
         </div>
-    </body>
-</html>
+    </div>
+@endsection
