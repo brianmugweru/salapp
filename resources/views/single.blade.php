0f775e810c9c58d945da013527439be861e5ba9d
Brian Mugweru <brianmugweru@gmail.com>
2018-02-21 13:40:04 +0300

    change likes section to allow like from user if he has not yet liked

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -16,17 +16,21 @@
 
                     <div class="container">
                         <div class="row">
-                            <div class="col-sm-4">
+                            <div class="col-sm-3">
                                 <h4>{{ $salon->name }}</h4>
                                 <img src="{{ Storage::url( $salon->image )}}" height="200" width="250" alt="found"/>
                             </div>
-                            <div class="col-sm-4" style="margin-top:50px;">
+                            <div class="col-sm-6" style="margin-top:50px;">
                                 <p>Opening time:<strong>  {{ $salon->opening_time }}</strong></p>
                                 <p>Closing time:<strong>  {{ $salon->closing_time }}</strong></p>
                                 <p>latitude: <strong> {{ $salon->latitude }}</strong></p>
                                 <p>longitude: <strong> {{ $salon->longitude }}</strong></p>
                                 @auth
-                                    <p><a href="/like/{{ $salon->id }}/{{ auth()->user()->id }}">like</a></p>
+                                    @if ($salon->likes[0]->user_id == auth()->user()->id and $salon->likes[0]->salon_id == $salon->id)
+                                        <p><i class="fa fa-heart"></i>already liked</p>
+                                    @else
+                                        <p><a href="/like/{{ $salon->id }}/{{ auth()->user()->id }}">{{ $salon->likes }}</a></p>
+                                    @endif
                                 @endauth
 
                             </div>
