7426ef1c5fe5a120c01b78cda6198d697d8ab291
Brian Mugweru <brianmugweru@gmail.com>
2018-02-21 13:35:34 +0300

    add gates and policies to prevent salon resources outflow to non-owner users

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\User;
+use App\Salon;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        'App\Model' => 'App\Policies\ModelPolicy'
     ];
 
     /**
@@ -25,6 +27,11 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('view-salon', function(User $user, Salon $salon){
+            return $user->id === $salon->user_id;
+        }); 
+        Gate::define('edit-salon', function(User $user, Salon $salon){
+            return $user->id === $salon->user_id;
+        });
     }
 }
