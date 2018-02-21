bc63e30e5446af73cb9fd9d8e07ea62a41d137df
Brian Mugweru <brianmugweru@gmail.com>
2018-02-21 13:42:33 +0300

    add script partial for timepicker jquerys

---
+++ resources/views/partials/timescript.blade.php
@@ -0,0 +1,6 @@
+<script>
+$('#weekdaystart').timepicker({startTime:'6:00am'});
+$('#weekdayend').timepicker({startTime:'5:00pm'});
+$('#weekendstart').timepicker({startTime:'7:00am'});
+$('#weekendstop').timepicker({startTime:'5:00pm'});
+</script>
