2cd391883945971824c0b0fb2f434f559d29cbfb
Brian Mugweru <brianmugweru@gmail.com>
2018-02-22 11:00:18 +0300

    add public table variable to tell object which table to use

--- app/Booking.php
+++ app/Booking.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class Booking extends Pivot
 {
-    //
+    public $table = 'bookings';
+
+    protected $fillable = ['user_id', 'salon_id'];
+
+    public function salon(){
+        return $this->belongsTo('App\Salon');
+    }
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
 }
