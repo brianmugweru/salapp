cfe8ec45e0469486a20fcb247b16abbb76f01ed6
Brian Mugweru <brianmugweru@gmail.com>
2018-02-22 11:11:24 +0300

    add links to salon offering style in styles page

--- resources/views/styles.blade.php
+++ resources/views/styles.blade.php
@@ -5,8 +5,10 @@
         <div class="row">
             @foreach($styles as $style)
             <div class="col-sm-3">
+                <a href="salon/{{ $style->salon->id }}/book">
                     <img src="{{ Storage::url($style->image) }}" width="100%" height="200">
                     <p style="text-align:left;">{{ $style->name }},&nbsp{{ $style->time_taken}}</br> {{ $style->salon->name }}</p>
+                </a>
             </div>
             @endforeach
         </div>
