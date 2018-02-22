a13d80326f2cf4aea6d32265b3efe7eb6975c2f8
Brian Mugweru <brianmugweru@gmail.com>
2018-02-22 11:12:04 +0300

    remove all unnecessary login and registration routes and use auth routes from laravel generic

--- routes/web.php
+++ routes/web.php
@@ -24,30 +24,14 @@ Route::get('/liked/salons', 'HomeController@getlikedsalons');
 
 Route::get('/styles', 'HomeController@getstyle');
 
-/* 
- * REGISTRATION ROUTES
- *
- */
-Route::get('/auth/register','RegistrationController@create')->name('register');
-
-Route::post('/auth/register', 'RegistrationController@store')->name('register');
-
+Route::get('/salon/book/{id}', 'HomeController@booksalon');
 
 /*
- * SESSION ROUTES
- *
+ * AUTH ROUTES
  */
-Route::get('/auth/login','SessionController@create')->name('login');
+Auth::routes();
 
-Route::post('/auth/login', 'SessionController@authenticate')->name('login');
-
-Route::get('/auth/logout', 'SessionController@destroy')->name('logout');
-
-Route::get('/password/reset', 'SessionController@passwordreset')->name('password.request');
-
-Route::post('/password/email', 'SessionController@sendresetlink')->name('password.email');
-
-
+Route::get('/home', 'HomeController@redirect')->name('home');
 /*
  * SALON ROUTES WITH ISSALON MIDDLEWARE PROTECTION
  *
@@ -80,3 +64,4 @@ Route::group(['middleware' => 'issalon'], function(){
 
     Route::resource('/service/', 'ServiceController');
 });
+
