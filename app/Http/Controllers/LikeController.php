4e2769bd72087ccdb9b4b24b1662780ae6d76cf4
Brian Mugweru <brianmugweru@gmail.com>
2018-02-25 11:17:14 +0300

    add methods likesalons and getliked salons transferred from homecontroller

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -4,7 +4,48 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Salon;
+
+use App\User;
+
+use App\Like;
+
 class LikeController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance for authentication middleware.
+     *
+     * @return void
+     */
+ 
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    public function likeSalon($salon_id)
+    {
+        $salon = Salon::find($salon_id);
+
+        $salon->addRank();
+
+        $like = new Like;
+
+        $like->user_id = auth()->user()->id;
+
+        $like->salon_id = $salon_id;
+
+        $like -> save();
+
+        return redirect('/salon/'.$salon_id);
+    }
+
+    public function salons()
+    {
+        $likes = Like::where('user_id', auth()->user()->id)->get();
+
+        return view('liked')->withLikes($likes);
+    }
+
 }
