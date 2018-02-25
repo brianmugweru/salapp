d0b955e1362eb75a891b39a8858c2efb621710ad
Brian Mugweru <brianmugweru@gmail.com>
2018-02-25 11:28:54 +0300

    add hyphen btwn opening time and salon closing time

--- resources/views/liked.blade.php
+++ resources/views/liked.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             @foreach($likes as $like)
             <div class="col-sm-3">
-                <a href="/salon/{{ $like->salon->id }}/book">
+                <a href="/salon/{{ $like->salon->id }}">
                     <img src="{{ Storage::url($like->salon->image) }}" width="100%" height="200">
                     <p style="text-align:left;">{{ $like->salon->name }}</br> {{ $like->salon->opening_time }}-{{ $like->salon->closing_time }}</p>
                 </a>
