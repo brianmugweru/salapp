2be3d9a6c97c2105e9f55b19bb30e1743648f989
Brian Mugweru <brianmugweru@gmail.com>
2018-02-25 11:15:24 +0300

    add method index, create and store to booking controller for salon booking

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -6,5 +6,42 @@ use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance for authentication middleware.
+     *
+     * @return void
+     */
+ 
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['get']);
+    }
+
+    /*
+     * RENDERS ALL OF USERS BOOKINGS TO PAGE
+     */
+    public function index()
+    {
+        $bookings = Booking::where('user_id', auth()->user()->id)->get();
+
+        return view('booking.index', compact('bookings'));
+    }
+    /*
+     * RENDERS BOOKING FORM FOR USER TO FILL IN SPECIFICS AND BOOK
+     */
+    public function create($salon_id)
+    {
+        $salons = Salon::find($salon_id);
+
+        return view('salon.index')->withSalons($salons);
+    }
+
+    /*
+     * STORES BOOKING FIELDS FROM USER INTO DATABASE
+     */
+    public function store(Request $request, $salon_id)
+    {
+
+    }
+
 }
