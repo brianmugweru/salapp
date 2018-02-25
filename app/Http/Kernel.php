69df2de8a68ee79971d923f5c0150fcd0485fd5c
Brian Mugweru <brianmugweru@gmail.com>
2018-02-25 11:22:09 +0300

    change issalon middleware registration to isSalon

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,6 +57,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'issalon' => \App\Http\Middleware\IsSalon::class,
+        'isSalon' => \App\Http\Middleware\IsSalon::class,
     ];
 }
