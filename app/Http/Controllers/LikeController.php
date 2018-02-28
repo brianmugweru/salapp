d48e2a4ad36e4d52ab91ccc7ee17f3de9a94dad6
Brian Mugweru <brianmugweru@gmail.com>
2018-02-28 13:58:44 +0300

    add withLikes to get user salons

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -23,24 +23,6 @@ class LikeController extends Controller
         $this->middleware('auth');
     }
 
-
-    public function likeSalon($salon_id)
-    {
-        $salon = Salon::find($salon_id);
-
-        $salon->addRank();
-
-        $like = new Like;
-
-        $like->user_id = auth()->user()->id;
-
-        $like->salon_id = $salon_id;
-
-        $like -> save();
-
-        return redirect('/salon/'.$salon_id);
-    }
-
     public function salons()
     {
         $likes = Like::where('user_id', auth()->user()->id)->get();
