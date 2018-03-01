e420b7bfca9ff6e1fbca6415053764bb987d6e1b
Brian Mugweru <brianmugweru@gmail.com>
2018-03-02 00:20:08 +0300

    separate map-picker blade template for reusability

---
+++ resources/views/partials/map-picker.blade.php
@@ -0,0 +1,21 @@
+
+<div id="myModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+    <!-- Modal content-->
+        <div class="modal-content" style="border-radius:10px">
+            <div class="modal-header" style="border-radius:10px">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Modal Header</h4>
+            </div>
+            <div class="modal-body" style="width:570px;height:400px;"> </div>
+            <form class="form-horizontal" action="mapinputs" method="post" style="margin-bottom:20px">
+                <div class="form-group">
+                    <label class="col-sm-1 control-label" style="color:black">location:</label>
+                    <div class="col-sm-5"><input type="text" class="form-control" id="map-address" placeholder="Enter a location" autocomplete="off"></div>
+                    <div class="col-sm-6"><p>Drag the marker to select location</p></div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
