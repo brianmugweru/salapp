c39f85d0a6c448b2d7d1441f008f4a24595800c0
Brian Mugweru <brianmugweru@gmail.com>
2018-03-02 16:27:21 +0300

    add javascript method for previewing salon locations on map, add listener to display salon infowindow on map onclick, add idle listener to get map bounds and call function to request salons within the bounds, add function getBounds, and add hoverin and hoverout listener function to change marker style on hover in and out of salons, add infowindow function to get salon info on marker click

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+
     <div class="container">
         <div class="row">
             <div class="col-md-6">
@@ -12,12 +13,12 @@
             </div>
         </div><br>
         <div class="row">
-            <div class="col-sm-7">
-                <div class="row">
+            <div class="col-sm-7" style="height:600px; overflow-y:auto;">
+                <div class="row" id="eachloop">
                     @foreach($salons as $salon)
                     <div class="col-sm-6">
                         <a href="/salon/{{ $salon->id }}" onmouseout="hoverOut({{$salon}})" onmouseover="hoverListener({{$salon}})">
-                            <img src="{{ Storage::url($salon->image) }}" width="100%" height="200">
+                            <img src="{{ $salon->image }}" width="100%" height="200">
                             <p style="text-align:left;">{{ $salon->name }}</br> {{ $salon->rank }}</p>
                         </a>
                     </div>
@@ -29,6 +30,7 @@
             </div>
         </div>
     </div>
+
 <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
 <script>
     var map;
@@ -36,19 +38,31 @@
     var markers = [];
     var infoWindow = [];
     var position = [];
+    var mylat;
+    var mylon;
+
+    navigator.geolocation.getCurrentPosition(showPosition);
+    function showPosition(position){
+        mylat = position.coords.latitude;
+        mylon = position.coords.longitude;
+
+        console.log('mylat:'+mylat+',mylng:'+mylon);
+    }
+    console.log('mylat:'+mylat+',mylng:'+mylon);
 
     function initMap(){
 
         map = new google.maps.Map(document.getElementById('map'),{
-            zoom: 12,
-            center:new google.maps.LatLng(-1.35645,36.75555),
+            zoom: 13,
+            center:new google.maps.LatLng(-1.28755,36.7899),
             mapTypeId:'roadmap'
         });
 
         salons.forEach(function(salon){
             var marker = new google.maps.Marker({
                 position:new google.maps.LatLng(salon.latitude,salon.longitude),
-                map:map
+                map:map,
+                salonId:salon.id
             });
 
             markers.push(marker);
@@ -68,16 +82,37 @@
         });
 
         map.addListener('idle', function(){
-            console.log('\n');
             for(var i = 0; i<markers.length; i++){
                 if(map.getBounds().contains(markers[i].getPosition())){
-                    position.push('lat[]='+markers[i].getPosition().lat());
+                    position.push('lat='+markers[i].salonId);
                 }else{
                 }
             }
-            console.log(position);
-            //windows.location = 'localhost:8000/?'+position;
+            //getBounds();
+            //window.location = 'location:8000/query?'+position.join('&');
             position.length = 0;
+        });
+    }
+
+    function getBounds(){
+        var bounds = map.getBounds();
+        var areaBounds = {
+            North: bounds.getNorthEast().lat(),
+            South: bounds.getSouthWest().lat(),
+            East: bounds.getNorthEast().lng(),
+            West: bounds.getSouthWest().lng()
+        }
+        $.get('http://localhost:8000/?minlat='+areaBounds.North+'&maxlat='+areaBounds.South+'&minlng='+areaBounds.West+'&maxlng='+areaBounds.East, function(data, status){
+            $('#eachloop').empty();
+            $.each(data, function(index, salon) {
+                var item = '<div class="col-sm-6">'+
+                    '<a href="/salon/'+salon.id+'" onmouseout="hoverOut('+salon+')" onmouseover="hoverListener('+salon+')">'+
+                        '<img src="' + salon.image + '" width="100%" height="200"/>'+
+                        '<p style="text-align:left;">'+ salon.name +'</br>'+ salon.rank +'</p>'+
+                    '</a>'+
+                '</div>';
+                $('#eachloop').append(item);
+            });
         });
     }
 
@@ -122,11 +157,11 @@
             }
         }
     }
-    function infoWindowText(salon) {
+   function infoWindowText(salon) {
         var content ='<div id="panel panel-default">'+
                      '<div class="panel-heading"><h5>'+salon.name+'</h5></div>' +
                      '<div class="panel-body">' +
-                     '<img src="'+salon.image+'" alt="'+salon.name+'" height="115" width="100%">' +
+                     '<img src=http://localhost:8000/'+salon.image+' alt="'+salon.image+'" height="115" width="100%">' +
                      '<div>Opening Time<strong>'+salon.opening_time+'</strong></div><br>' +
                      '<div>Closing Time<strong>'+salon.closing_time+'</strong></div><br>' +
                      '<div>Rank<strong>'+salon.rank+'</strong></div>' +
